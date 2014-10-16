1a4b1feb22d7f999bd78104bcc29f196c280c902
meabhisek <meabhisek@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-16 20:38:20 +0000

    Info update
    git-svn-id: https://plugins.svn.wordpress.org/mini-membership/trunk@1008814 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mini-membership.php
+++ mini-membership.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Mini Membership
 Plugin URI: http://increasy.com
-Description: Give subscribers private content read access and turn your WordPress site into a mini membership site.Just ask visitors to register as Subscriber to read Private Content, or call it Members Only Content.This plugin also blocks dashboard access for subscribers(even if they try /wp-admin/), hides admin bar from front end and adds a login/logout link to your site's primary menu.No fancy settings or features, just activate and have a mini membership site to attract more subscribers.
+Description: Give subscribers private content read access and turn your WordPress site into a mini membership site.Just ask visitors to register as Subscriber to read Private Content, or call it Members Only Content.This plugin also blocks dashboard access for subscribers(even if they try /wp-admin/), hides admin bar from front end and adds an optional widget to let people register, log in and log out of your site.No fancy settings or features, just activate and have a mini membership site to attract more subscribers.
 Author: Kumar Abhisek
 Author URI: http://increasy.com
 Version:1.0
